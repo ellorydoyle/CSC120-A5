1676329f9a7de4f3061b22856fc714acd6a71c29
Ellory <edoyle@smith.edu>
2023-03-09 21:26:37 -0500

    Almost Complete

--- Car.java
+++ Car.java
@@ -1,3 +1,47 @@
+import java.util.ArrayList;
 public class Car {
+
+    ArrayList<Passenger> passengersAboard;
+    int maxCapacity;
+
+    public Car(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+        this.passengersAboard = new ArrayList<Passenger>(maxCapacity);
+    }
+
+    public int getCapacity(){
+        return maxCapacity;
+    }
+
+    public int seatsRemaining(){
+        return (maxCapacity - passengersAboard.size());
+    }
+
+    public void addPassenger(Passenger p){
+        getCapacity();
+        if (passengersAboard.size() == maxCapacity){
+            throw new RuntimeException("The car is at capacity.");
+        }
+        passengersAboard.add(p);
+    }
+
+    public void removePassenger(Passenger p){
+        if (!passengersAboard.contains(p)){
+            throw new RuntimeException("This passenger is not in the car.");
+        }
+
+        passengersAboard.remove(p);
+    }
+
+    public void printManifest(){
+        System.out.println("CAR MANIFEST");
+        if (passengersAboard.isEmpty()){
+            System.out.println("This car is EMPTY.");
+        }
+        else {passengersAboard.forEach(passenger ->{
+            System.out.println(passenger);
+        });
+        }
+    }
 
 }
--- Engine.java
+++ Engine.java
@@ -1,3 +1,33 @@
 public class Engine {
 
+    FuelType FuelType;
+    double currentFuelLevel;
+    double maxFuelLevel;
+
+    public Engine(FuelType FuelType, double currentFuelLevel, double maxFuelLevel){
+        this.FuelType = FuelType;
+        this.currentFuelLevel = currentFuelLevel;
+        this.maxFuelLevel = maxFuelLevel;
+    }
+
+    public Engine(FuelType fuelType2, double fuelCapacity, int nCars, int passengerCapacity) {
+    }
+
+    public Engine(FuelType fuelType2, double fuelCapacity, int nCars, int passengerCapacity, int seatsRemaining) {
+    }
+
+    public void refuel(){
+        if (currentFuelLevel == maxFuelLevel){
+            throw new RuntimeException("The current fuel level is already at its maximum.");
+        }
+        currentFuelLevel = maxFuelLevel;
+        System.out.println("The fuel level has been successfully returned to its maximum.");
+    }
+
+    public void go(){
+        if (currentFuelLevel == 0){
+            throw new RuntimeException("There is no fuel. Please refuel immediately.");
+        }
+        currentFuelLevel = (currentFuelLevel - 5.0);
+    }
 }
