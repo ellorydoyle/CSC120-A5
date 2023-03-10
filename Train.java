1d4c5a331a23be213e196209855f22ea5308ab5e
Ellory <edoyle@smith.edu>
2023-03-09 21:27:00 -0500

    Almost Entirely Complete

--- Train.java
+++ Train.java
@@ -1,3 +1,50 @@
+import java.util.ArrayList;
 public class Train {
 
+    private final Engine engine;
+    private ArrayList<Car> cars;
+    FuelType fuelType;
+    double fuelCapacity;
+    int nCars;
+    int passengerCapacity;
+    int seatsRemaining;
+
+    public Train(FuelType fuelType, double fuelCapacity, int nCars, int passengerCapacity) {
+        this.engine = new Engine(fuelType, fuelCapacity, nCars, passengerCapacity);
+        this.cars = new ArrayList<Car>();
+    }
+
+    public Engine getEngine(){
+        return engine;
+    }
+    
+    public Car getCar(int i){
+        return cars.get(i);
+    }
+    
+    public int getMaxCapacity(){
+        for (int i = 0; i < nCars; i++){
+            getCar(i);
+            passengerCapacity += cars.get(i).maxCapacity;
+        }
+        return passengerCapacity;
+    }
+
+    public int seatsRemaining(){
+        getMaxCapacity();
+        seatsRemaining = passengerCapacity;
+        for (int i = 0; i < nCars; i++){
+            seatsRemaining = seatsRemaining - cars.get(i).passengersAboard.size();
+        }
+        return seatsRemaining;
+    }
+
+    public void printManifest(){
+        System.out.println("TRAIN MANIFEST");
+        for (int i=0; i < nCars; i++){
+            cars.get(i).passengersAboard.forEach(passenger ->{
+                System.out.println(passenger); 
+            });
+        }
+    }
 }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,9 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Association` affects your programming?
+
+    *I find that it is fairly confusing for me to understand association because it seems like a natural way of coding, and I felt like it would be more difficult to grasp. Because of that, identifying what it looks like in code is not the easiest task for me.*
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+    *I'm pretty sure that all of it works so far, but I had a difficult time formatting Train. I would give the advice to someone who is taking the course that they should learn how to make proper constructors.*
